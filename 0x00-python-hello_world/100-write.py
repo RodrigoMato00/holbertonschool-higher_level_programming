4cccef6696d72b433d8cee2a87490e00d73218eb
rodrigomato00 <3293@holbertonschool.com>
2021-09-06 20:32:54 +0000

    arrleglo 101 y 100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import sys
-sys.tderr.write('and that piece of art is useful - Dora Korpar, , 2015-10-19')
+sys.stderr.write('and that piece of art is useful - Dora Korpar, , 2015-10-19')
 exit (1)
