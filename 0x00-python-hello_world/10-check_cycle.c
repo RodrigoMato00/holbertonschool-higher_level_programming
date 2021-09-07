667e4b1fe4fd36e1fc8ed905cc4ee9614a5292ff
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-06 22:49:43 -0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,9 +10,6 @@ int check_cycle(listint_t *list)
 	listint_t *tortuga;
 	listint_t *liebre;
 
-	/*if (list == NULL || list->next == NULL)
-		return (0);*\
-
 	tortuga = list->next;
 	liebre = list->next->next;
 
