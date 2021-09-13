9efb749136e5e3e0b425ae85a2e27aca3cab76f4
rodrigomato00 <3293@holbertonschool.com>
2021-09-13 21:13:18 +0000

    cambios 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str_cp = ""
-    if (my_string is None):
-        return
     for a in my string:
-        if (a != 'c' and a != 'C')
-        str_cp += (a)
+        if a != "c" and a != "C":
+            str_cp += a
     return str_cp
