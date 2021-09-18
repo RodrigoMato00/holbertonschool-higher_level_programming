7583b8475d474532f7c687576177ed9eeb6107bc
RodrigoMato00 <3293@holbertonschool.com>
2021-09-18 16:18:24 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,25 +1,53 @@
 #include "lists.h"
 
 /**
- *is_palindrome - checks if a singly linked list is a palindrome
- *@head: head of a linked list
- *Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ *listint_t - ldkfjdkfjpdofkpdokdpokcsdpokcsdpokcsodpkcdopk
+ *@head: sfldfkamdfamdfkadmzfkamdklmclddfadfdfad
+ *@i: dlkfjadiofjaoidjfaiodjf
+ *Return: pfkspodkspodkspdokspd
+ */
+listint_t *check_list(listint_t *head, listint_t *i)
+{
+	listint_t *j;
+
+	if (i == NULL)
+		return (head);
+
+	else
+	{
+		j = check_list(head, i->next);
+
+		if (j == NULL)
+			return (NULL);
+
+		if (j->n == i->n && j->next)
+			return (i->next);
+
+		else if (j->n == head->n)
+			return (j);
+
+		else
+			return (NULL);
+	}
+	return (NULL);
+}
+
+/**
+ *is_palindrome - dfjkdopfkpdfksdf
+ *@head: odfjpdskfpodkfposdkf
+ *Return: dfñksñdmfslkdmfslkmd
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int num;
 	listint_t *i;
 
-	if (!head || !*head)
+	if (head == NULL)
+		return (0);
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-
 	i = *head;
-	num = 1;
-
-	while (i)
-	{
-		i = i->next;
-		num++;
-	}
-	return (0);
+	if (check_list(*head, i))
+		return (1);
+	else
+		return (0);
 }
