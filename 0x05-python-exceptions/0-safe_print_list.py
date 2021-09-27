2c045b76957f03be317e19940f86aa5e2d596ffd
RodrigoMato00 <3293@holbertonschool.com>
2021-09-27 06:56:41 -0700

    termine antes de las 11

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for a in range(x):
+            print("{}".format(my_list[a]), end ="")
+    except IndexError:
+        return a
+    else:
+        return x
+    finally:
+        print("")
