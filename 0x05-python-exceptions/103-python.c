1814997b7b8e5741335747d9af5d4d605f8208c1
rodrigomato00 <3293@holbertonschool.com>
2021-09-27 19:31:41 +0000

    103 no terminado

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,29 @@
+#include <python.h>
+
+Void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ *print_python_list - ljAlkwjfwljwj
+ *@p: wjkdfoidjdmciodm
+ */
+Void print_python_list(PyObject *p)
+{
+}
+
+/**
+ *print_python_bytes - lddkfjldfalkmak
+ *@p: ldjsdcidmcsd
+ */
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ *print_python_float - ldjfoidfjoj
+ *@p: nkvslfnvsndlsjdn
+ */
+void print_python_float(PyObject *p)
+{
+}
