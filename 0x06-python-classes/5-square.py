5b35ba34b63faeb6d7641baf550fed469f2026c4
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-28 20:31:31 -0300

    Más comentarios

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
+"""Algo algo
+   Alguien paso por aqui
+"""
+
+
 class Square:
+    """No me gustan los comentarios"""
     def __init__(self, size=0):
+        """Comentario"""
         self.__size = size
     @property
     def size(self):
+        """Alguien o algo"""
         return(self.__size)
     @size.setter
     def size(self, value):
+        """Comentar es para tontos"""
         self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
     def area(self):
+        """Hola hola"""
         return(self.__size ** 2)
     def my_print(self):
+        """Algo algote"
         if self.__size == 0:
             print()
         for i in range(self.__size):
