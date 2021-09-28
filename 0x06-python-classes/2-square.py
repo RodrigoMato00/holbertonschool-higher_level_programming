164c6ebf6b04bf3219298002780cdc6e55c46c3d
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-28 20:05:54 -0300

    Arreglo

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,4 +7,8 @@
 class Square:
     """Algo algo """
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
