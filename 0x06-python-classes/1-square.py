4c272c031ba85a80ee7d3b2ef2c06a461ad726d1
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-28 20:00:54 -0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""Algo algo"""
+"""
+    Algo algo algo algo algo
+    Algo
+"""
 
 
 class Square:
-    def __init__(self, size=0):
+    """
+    Algo algo algo
+    """
+
+    def __init__(self, size):
         self.__size = size
