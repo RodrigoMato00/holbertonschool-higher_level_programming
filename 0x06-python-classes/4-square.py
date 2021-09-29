7964be368f7d03d33ab9da8ea1ea8c19509bebb7
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-28 21:51:07 -0300

    Corrección 1b

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,12 +9,12 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-    
+
     @property
     def size(self):
         """Algo algo algo"""
         return(self.__size)
-    
+
     @size.setter
     def size(self, value):
         """Algo algo algo"""
