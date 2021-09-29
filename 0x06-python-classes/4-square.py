056c43ab0189a0c74fd0bcf6760c73227bcbdccc
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-09-28 21:50:00 -0300

    Corrección 1a

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,12 +6,15 @@
 
 class Square:
     """Algo Alguito"""
+
     def __init__(self, size=0):
         self.__size = size
+    
     @property
     def size(self):
         """Algo algo algo"""
         return(self.__size)
+    
     @size.setter
     def size(self, value):
         """Algo algo algo"""
@@ -20,6 +23,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         """Algo"""
         return(self.__size ** 2)
