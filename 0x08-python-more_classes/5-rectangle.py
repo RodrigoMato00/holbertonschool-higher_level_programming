a62f4f06a249e5a3c51e67814aafa5613ce994d8
rodrigomato00 <3293@holbertonschool.com>
2021-10-05 02:37:03 +0000

    correccion 4

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -10,9 +10,11 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -20,9 +22,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -30,8 +34,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+
     def __str__(self):
         t = ""
         for a in range(self.__height):
@@ -39,10 +47,13 @@ class Rectangle:
             if a is not self.__height - 1:
                 t += "\n"
         return t
+
     def __del__(self):
         print("Bye rectangle...")
+
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if self.__width is 0 or self.__height is 0:
             return 0
