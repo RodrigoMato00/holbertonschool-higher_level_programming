bde3369b9b1c3cdc5dad32643b30cc6e3d9dee35
rodrigomato00 <3293@holbertonschool.com>
2021-10-05 00:01:48 +0000

    agregando comentarios

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Modulo Rectangle
+"""
+
+
 class Rectangle:
+    """creamos la
+    clase Rectangle"""
     def __init__(self):
         self
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""modulo Rectangle
+"""
 
 class Rectangle:
-
+     """A nuestra clase Rectangle
+        le agregamos un largo y un ancho"""
      def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
