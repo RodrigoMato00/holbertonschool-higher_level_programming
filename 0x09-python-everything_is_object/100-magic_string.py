efad14a26398a9f376eb92082e48186b87d01fb3
rodrigomato00 <3293@holbertonschool.com>
2021-10-05 22:53:04 +0000

    correccion3

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(a=[]):
-    magic_string.a = getattr(magic_string, a, 0) + 1
+    magic_string.a = getattr(magic_string, 'a', 0) + 1
     return ", ".join(["BestSchool" for b in range(magic_string.a)])
