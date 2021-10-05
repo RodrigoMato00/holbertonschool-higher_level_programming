cb9e651578f1cbdd1d82d1e787fff1839ccd51e9
rodrigomato00 <3293@holbertonschool.com>
2021-10-05 00:36:56 +0000

    arreglo de lugares

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,21 +9,8 @@ class Rectangle:
     le agregamos un largo y un ancho"""
 
     def __init__(self, width=0, height=0):
-        self.__height = height
         self.__width = width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = height
 
     @property
     def width(self):
@@ -37,3 +24,16 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
