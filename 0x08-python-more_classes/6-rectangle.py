5657de1f3791c767349e8ac283646a3bc8fc466b
rodrigomato00 <3293@holbertonschool.com>
2021-10-06 01:02:14 +0000

    arreglo 6.3

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
        que ya tiene un largop y ancho ahora le
        agregamos el valculo del Area y del perimetro"""
 
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
 
