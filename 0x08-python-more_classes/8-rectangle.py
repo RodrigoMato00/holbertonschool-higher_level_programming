843d62d3adcf6460f6e0e8990ba4d4206a437119
rodrigomato00 <3293@holbertonschool.com>
2021-10-06 01:08:22 +0000

    8

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Modulo Rectangl
+"""
+
+
+class Rectangle:
+    """A nuestra clase rectangulo
+       que ya tiene un largop y ancho ahora le
+       agregamos el valculo del Area y del perimetro"""
