fa08004b7517c58dcbcc8a8a43cbb409117f00cc
rodrigomato00 <3293@holbertonschool.com>
2021-10-12 23:41:45 +0000

    arreglo 10.0

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,10 +4,13 @@ Module:
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
+"""class Rectangle"""
 
 
 class Square(Rectangle):
     """class Square
     """
     def __init__(self, size):
+        """init
+        """
         super().__init__(size, size)
