43e7883fc636b2e2123ce5bad4a98615012eb009
RodrigoMato00 <3293@holbertonschool.com>
2021-10-12 07:05:25 -0700

    1 terminado

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-
+Modulo lookup
 """
 
 
 def lookup(obj):
+    """returns the list of available attributes
+    and methods of an object
     """
-    """
-
+    list = dir(obj)
+    return list
     
