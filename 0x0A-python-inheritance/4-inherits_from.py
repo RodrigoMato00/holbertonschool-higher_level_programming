ab213260e011ec075d30bf36d7ef2351e6575a9a
RodrigoMato00 <3293@holbertonschool.com>
2021-10-12 09:23:49 -0700

    5 terminado

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class ; otherwise False.
+    """
+    if isinstance(obj, a_class) and type(obj)  is not a_class:
+        return True
+
+    else:
+        return False
