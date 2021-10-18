8b67b5c93c94a2f6909546fd57b924f9992c47ad
rodrigomato00 <3293@holbertonschool.com>
2021-10-18 22:03:54 +0000

    agregando

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,3 +17,52 @@ class Square(Rectangle):
         """
         """
         return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,  self.width))
+
+    @property
+    def size(self):
+        """
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        """
+
+        if len(args) > 0:
+
+            for i in range(len(args)):
+
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                    self.height = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+
+        else:
+
+            if len(kwargs) > 0:
+
+                k = kwargs.keys()
+
+                for i in k:
+
+                    if i == 'id':
+                        self.id = kwargs['id']
+                    if i == 'size':
+                        self.width = kwargs['size']
+                        self.height = kwargs['size']
+                    if i == 'x':
+                        self.x = kwargs['x']
+                    if i == 'y':
+                        self.y = kwargs['y']
