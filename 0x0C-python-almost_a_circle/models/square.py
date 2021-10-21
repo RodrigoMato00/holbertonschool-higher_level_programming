efbb3c16f8e55e7d7dd94516e0e14759dee16075
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-10-20 23:54:54 -0300

    Probando

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,41 +1,41 @@
 #!/usr/bin/python3
 """
-jfjdidcjdfido
+=========================================================
+Creation of the class Square that inherits from Rectangle
+=========================================================
 """
 
 from models.rectangle import Rectangle
-"""domcnidd
-"""
 
 
 class Square(Rectangle):
+    """ class Square inherits from Rectangle """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """kckdkxmcjfid
-        """
+        """ Constructor of Square """
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ododmcnfuriod
-        """
+        """Rodrigo"""
+
         return("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,  self.width))
 
     @property
     def size(self):
-        """kdxkcjfiod
-        """
+        """Rodrigo"""
+
         return self.width
 
     @size.setter
     def size(self, value):
-        """kifkcmncnfidod
-        """
+        """Rodrigo"""
+
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """mcndjdixkcjfif
-        """
+        """Rodrigo"""
 
         if len(args) > 0:
 
@@ -70,8 +70,7 @@ class Square(Rectangle):
                         self.y = kwargs['y']
 
     def to_dictionary(self):
-        """ncndifikdodie
-        """
+        """Rodrigo"""
 
         dictionary = {}
         dictionary['id'] = self.id
