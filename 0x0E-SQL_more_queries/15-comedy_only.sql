53303d043328c55b0ccf9d4cf5ad4742fd099dbb
RodrigoMato00 <3293@holbertonschool.com>
2021-11-17 14:56:14 -0800

    15 funcional

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -8,5 +8,5 @@ SELECT title
 FROM tv_genres
 LEFT JOIN tv_show_genres ON genre_id = tv_genres.id
 LEFT JOIN tv_shows ON show_id = tv_shows.id
-WHERE name = "comedy"
+WHERE name = "Comedy"
 ORDER BY title ASC;
