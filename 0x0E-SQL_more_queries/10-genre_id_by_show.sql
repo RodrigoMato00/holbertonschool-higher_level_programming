6ce48ed56251c6621cfe34542949cd1f8abc5c30
RodrigoMato00 <3293@holbertonschool.com>
2021-11-17 13:26:41 -0800

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_shows.title = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
 ORDER BY title, genre_id ASC;
