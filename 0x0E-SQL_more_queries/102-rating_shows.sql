ae96001bba2188ec921f142a6f38f4df5d0451d9
RodrigoMato00 <3293@holbertonschool.com>
2021-11-17 18:54:58 -0800

    102 probando checker

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,8 +4,8 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT title, SUM(tv_show_ratings.rate) as rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) as rating
 FROM tv_shows
-INNER JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show.id
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show.id
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
