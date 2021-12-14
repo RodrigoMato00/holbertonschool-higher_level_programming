8d33ff40b5fa717336683cb8d608e87aab240a7b
RodrigoMato00 <3293@holbertonschool.com>
2021-12-13 17:19:07 -0800

    10 funcionando

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function factorial (num) {
+  a = Number(num);
+  if (isNaN(num * (num - 1))) {
+    return (1);
+  } else if (num === 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+console.log(factorial(process.argv[2]));
