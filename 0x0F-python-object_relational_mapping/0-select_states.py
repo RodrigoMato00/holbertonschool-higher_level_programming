fc9254bfad11e01683d369454becabb1444ba46c
Rodrigomato00 <3293@holbertonschool.com>
2021-12-19 11:57:25 -0300

    correccion 1000

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     """
     connect to the database and select list of states in database
     """
-    data_connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    data_connect = MySQLdb.connect(host="localhost", port=3306,
+                                   user=sys.argv[1],
+                                   passwd=sys.argv[2], db=sys.argv[3])
     cur = data_connect.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id;")
     cur = cur.fetchall()
