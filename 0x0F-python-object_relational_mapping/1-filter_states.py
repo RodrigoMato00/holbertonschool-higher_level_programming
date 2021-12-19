58d6c91c447b45ef7a622e09e7b7f073a03e9131
Rodrigomato00 <3293@holbertonschool.com>
2021-12-19 13:02:04 -0300

    anda

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    """
+    connect to the database and select list of states in database
+    """
+    import MySQLdb
+    import sys
+
+    data_connect = MySQLdb.connect(host="localhost",
+                                   port=3306,
+                                   user=sys.argv[1],
+                                   passwd=sys.argv[2],
+                                   db=sys.argv[3])
+    cur = data_connect.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY 'N%'\
+                ORDER BY states.id")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    data_connect.close()
