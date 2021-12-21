58b4f3872f0e48415d617bf3723788a446d1d62c
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-12-21 10:02:39 -0300

    ident

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     import sys
 
     db = MySQLdb.connect(host="localhost",
-                                   port=3306,
-                                   user=sys.argv[1],
-                                   passwd=sys.argv[2],
-                                   db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC",
                 (sys.argv[4], ))
