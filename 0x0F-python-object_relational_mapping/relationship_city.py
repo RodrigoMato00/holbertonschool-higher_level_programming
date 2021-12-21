82f1114769e0fb43c25f94eb48f36dfd8b420040
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-12-21 14:00:08 -0300

    relationship prueba3

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+City class define
+"""
+
+from relationship_state import Base, State
+from sqlalchemy import Integer, String, ForeignKey, Column
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    City class id and name attributes
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
