05a539317a9550c63536487e0b2ccc9af41fff24
RodrigoMato00 <85580712+RodrigoMato00@users.noreply.github.com>
2021-12-21 11:45:25 -0300

    change

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,8 +5,6 @@ lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 import sys
-
-
 if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost",
@@ -22,5 +20,5 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-    
+
     
