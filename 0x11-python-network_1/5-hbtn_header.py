c9fa5c281dc5b6e1f1da70d253d1a105cb23679f
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 13:38:51 -0800

    5 arreglo

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,7 +4,7 @@ Python script that takes in a URL, sends a request to the URL
 and displays the value of the variable X-Request-Id in the response header
 """
 
-from  requests import get
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
