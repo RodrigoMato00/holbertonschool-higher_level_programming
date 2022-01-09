ae5c3eb48c6d4f260e5c530c207509a89b847a84
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 14:59:58 -0800

    10 pycode arreglo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes your GitHub credentials (username and password) and uses
-       the GitHub API to display your id.
+Python script that takes your GitHub credentials (username and password)
+       and uses the GitHub API to display your id.
 -The first argument will be your username
 -The second argument will be your password (in your case, a personal
         access token as password)
