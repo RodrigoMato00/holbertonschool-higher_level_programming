e7b90e7fa2ac6f2dc81d38235367a86559c3860c
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 06:24:39 -0800

    101 correccion -H

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script must send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request
-curl -sXH POST "Content-Type: application/json" "$1" -d @"$2"
+curl -sX POST -H "Content-Type: application/json" "$1" -d @"$2"
