eac9c6f92361e6d704cda575ddefd6d690a7aaf3
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 05:14:54 -0800

    6 andando

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    peak
+    """
+    length = len(list_of_integers)
+    if length == 0:
+        return None
+
+    half = length // 2
+    if (half == length - 1 or list_of_integers[half] >=
+            list_of_integers[half + 1]) and (half == 0 or
+                                             list_of_integers[half] >=
+                                             list_of_integers[half - 1]):
+        return list_of_integers[half]
+
+    if half != length - 1 and list_of_integers[half + 1]\
+    > list_of_integers[half]:
+        return find_peak(list_of_integers[half + 1:])
+
+    return find_peak(list_of_integers[:half])
