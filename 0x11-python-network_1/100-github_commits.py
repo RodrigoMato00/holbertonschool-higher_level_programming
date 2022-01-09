c450acb781921f44190e8424b0572b77c4023543
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 15:50:59 -0800

    100 arreglado pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,13 +9,16 @@ from requests import get
 from sys import argv
 
 if __name__ == "__main__":
-    init = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    init = "https://api.github.com/repos/{}/{}/commits"\
+            .format(argv[2], argv[1])
     r_g = get(init)
     commits = r_g.json()
 
     try:
         for count in range(10):
-            print("{}: {}".format(commits[count].get("sha"), commits[count].get("commit").get("author").get("name")))
+            print("{}: {}".format(commits[count]
+                  .get("sha"), commits[count].get("commit")
+                  .get("author").get("name")))
 
     except IndexError:
         pass
