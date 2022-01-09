a2c2e6e2ee267ab78d51067f4fd817100c20c84c
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 10:23:02 -0800

    networking hace mal

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.request as request
+from sys import argv
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as r:
+        print(r.headers.get('X-Request-Id'))
