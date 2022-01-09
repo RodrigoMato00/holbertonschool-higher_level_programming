ec349eccfea45413cbb7d91fb334e12422ddc0f3
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 12:07:03 -0800

    2 funcional

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,13 @@ Python script that takes in a URL and an email,
 sends a POST request to the passed URL with the email
 as a parameter, and displays the body of the response (decoded in utf-8)
 """
-
 import urllib.request as request
+import urllib.parse as parse
 from sys import argv
 
 if __name__ == "__main__":
-    with request.urlopen(argv[1]) as r:
-        print(r.headers.get('X-Request-Id'))
+    email = {'email': argv[2]}
+    comp = urllib.parse.urlencode(email).encode('ascii')
+    r_r = urllib.request.Request(argv[1], comp)
+    with urllib.request.urlopen(req) as r:
+        print(r.read().decode('utf-8'))
