7415bee7d4a919fff58d8a68b1ace9648aa920b8
RodrigoMato00 <3293@holbertonschool.com>
2022-01-09 12:30:57 -0800

    3 al probarlo se ejecuta pero solo muestra errores

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Send request
+"""
+
+import urllib.request as request
+import urllib.error as error
+from sys import argv
+
+if __name__ == "__main__":
+
+    r_r = request.Request(argv[1])
+    try:
+        with request.urlopen(r_r) as r:
+            print(r.read().decode("utf-8"))
+    except error.HTTPError as ur_err:
+        print("Error code: {}".format(ur_err.code))
