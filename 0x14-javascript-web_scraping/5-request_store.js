956e9d57998b537f99b1582aadb717bd5d7f0ddd
RodrigoMato00 <3293@holbertonschool.com>
2022-02-01 12:00:24 -0800

    LGF

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,7 +9,14 @@ Write a script that gets the contents of a webpage and stores it in a file.
 */
 
 const req = require('request');
+const fs = require('fs');
 
 req(process.argv[2], function (error, response, body) {
-    if (error) console.log(error)
-};
+  if (error) console.log(error);
+
+  else {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
+       if (error) console.log(error);
+    });
+  }
+});
