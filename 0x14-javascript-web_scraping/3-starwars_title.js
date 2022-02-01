c1017d2d156ce05d93289dbf0069df2a9bbacf87
RodrigoMato00 <3293@holbertonschool.com>
2022-02-01 09:21:47 -0800

    LGF

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,12 +9,12 @@ Write a script that prints the title of a Star Wars movie where the episode numb
 */
 
 const req = require('request');
-const api_url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const api_url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 req(api_url, function (error, response, body) {
-    if (error) console.log(error);
+  if (error) console.log(error);
 
-    else if (response.statusCode === 200) {
-	console.log(JSON.parse(body).title);
-    }
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
 });
