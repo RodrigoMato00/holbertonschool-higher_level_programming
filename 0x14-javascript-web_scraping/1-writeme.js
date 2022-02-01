3cccce1697d6e38ca48468a7f87415c69e48a154
RodrigoMato00 <3293@holbertonschool.com>
2022-02-01 13:16:48 -0300

    com

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 /*
-Write a script that prints all characters of a Star Wars movie:
--The first argument is the Movie ID - example: 3 = “Return of the Jedi”
--Display one character name by line in the same order of the list “characters” in the /films/ response
--You must use the Star wars API
--You must use the module request
+Write a script that writes a string to a file.
+-The first argument is the file path
+-The second argument is the string to write
+-The content of the file must be written in utf-8
+-If an error occurred during while writing, print the error object
 */
 const name = process.argv[2];
 
@@ -13,6 +13,6 @@ const content = process.argv[3];
 
 const fs = require('fs');
 
-fs.writeFile(name, content, 'utf-8', function (error) {
-  if (error) console.log(error);
+fs.writeFile(name, content, 'utf-8', function(error) {
+    if (error) console.log(error);
 });
