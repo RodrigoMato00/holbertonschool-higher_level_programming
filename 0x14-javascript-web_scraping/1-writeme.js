c53a5af013940db43356fc108b8d07663c4f540f
RodrigoMato00 <3293@holbertonschool.com>
2022-02-01 13:20:49 -0300

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -8,11 +8,8 @@ Write a script that writes a string to a file.
 -If an error occurred during while writing, print the error object
 */
 const name = process.argv[2];
-
 const content = process.argv[3];
-
 const fs = require('fs');
-
 fs.writeFile(name, content, 'utf-8', function(error) {
     if (error) console.log(error);
 });
