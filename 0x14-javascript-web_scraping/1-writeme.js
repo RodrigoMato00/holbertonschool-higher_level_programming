00c557f4806d00b0fcb9a4ac2af759e708ab7fdf
RodrigoMato00 <3293@holbertonschool.com>
2022-02-01 08:41:17 -0800

    probando

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
 /*
-Write a script that writes a string to a file.
+Writee a script that writes a string to a file.
 -The first argument is the file path
 -The second argument is the string to write
 -The content of the file must be written in utf-8
 -If an error occurred during while writing, print the error object
 */
-const name = process.argv.slice(2)[0];
-const content = process.argv.slice(2)[1];
 const fs = require('fs');
-fs.writeFile(name, content, 'utf-8', function (error) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (error) {
   if (error) console.log(error);
 });
